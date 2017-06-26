a54a493d858eb4d512fae7259219b86e9caa6a31
vagrant <vagrant@vagrant.vm>
2017-06-26 21:42:21 +0000

    added replay option

--- guessing-game/game.py
+++ guessing-game/game.py
@@ -4,24 +4,29 @@
 
 import random  
 
-num = random.randint(1,101)
-name = raw_input("Howdy, what's your name? ")
-guesses = 0
-guess = 0
+def guessing_game():
+	num = random.randint(1,101)
+	name = raw_input("Howdy, what's your name? ")
+	guesses = 0
+	guess = 0
 
-print name + ", I'm thinking of a number between 1 and 100"
-print "Try to guess my number."
+	print name + ", I'm thinking of a number between 1 and 100"
+	print "Try to guess my number."
 
-while guess != num:
-	guess = int(raw_input("Your guess? "))
-	guesses += 1 
-	if guess > num:
-		print "That's too high."
-	elif guess < num:
-		print "That's too low."
-	else:
-		print "You got it in", guesses, "guesses"
-		
+	while guess != num:
+		guess = int(raw_input("Your guess? "))
+		guesses += 1 
+		if guess > num:
+			print "That's too high."
+		elif guess < num:
+			print "That's too low."
+		else:
+			print "Good job! You got it in", guesses, "guesses!"
+			play = raw_input("Would you like to play again? Y or N ")
+			if play == "y".upper():
+				guessing_game()
+			else: 
+				break
 
-
+guessing_game()
 
