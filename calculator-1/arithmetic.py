33d4f084820da106cf071d83c391f75bcf2fd117
vagrant <vagrant@vagrant.vm>
2017-06-26 23:03:35 +0000

    calculator-1

---
+++ calculator-1/arithmetic.py
@@ -0,0 +1,44 @@
+"""Math functions for calculator."""
+
+
+def add(num1, num2):
+    """Return the sum of the two input integers."""
+    sum = num1 + num2 
+    return sum
+
+
+def subtract(num1, num2):
+    """Return the second number subtracted from the first."""
+    sum = num1 - num2
+    return sum
+
+def multiply(num1, num2):
+    """Multiply the two inputs together."""
+    sum = num1 * num2
+    return sum
+
+def divide(num1, num2):
+    """Divide the first input by the second, returning a floating point."""
+    sum = num1 / float(num2)
+    return sum
+
+def square(num1):
+    """Return the square of the input."""
+    sum = num1 * num1
+    return sum
+
+def cube(num1):
+    """Return the cube of the input."""
+    sum = num1 * num1 * num1
+    return sum
+
+def power(num1, num2):
+    """Raise num1 to the power of num and return the value."""
+    sum = num1 ^ num2
+    return sum
+
+def mod(num1, num2):
+    """Return the remainder of num / num2."""
+    sum = num1/num2
+    return sum
+
